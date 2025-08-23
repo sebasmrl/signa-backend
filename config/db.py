ec0d8c22a4e81c1c8bc3f390948fca4a672da181
Sebastian Morales <davesebastian99@gmail.com>
2025-08-22 21:15:01 -0500

    Added: DATABASE_URL env

--- config/db.py
+++ config/db.py
@@ -5,7 +5,9 @@ sqlite_filename = '../db/database.sqlite'
 base_dir = os.path.dirname(os.path.realpath(__file__))
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_filename)}"
 
+DATABASE_URL = os.getenv("DATABASE_URL", database_url)
+
 #Engine
-engine = create_engine(database_url)
+engine = create_engine(DATABASE_URL)
 meta = MetaData()
 conn = engine.connect()

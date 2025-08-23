5487a6a670fdbfae5ce15da128fe5e014291919f
Sebastian Morales <davesebastian99@gmail.com>
2025-08-22 20:12:04 -0500

    Added: .env file for CORS

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.brand import brand
 
 app = FastAPI()
+
+
+load_dotenv()
+frontend_domain = os.getenv("ALLOW_DOMAIN") 
+
+origins = [
+    frontend_domain,
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,           
+    allow_methods=["*"],              
+    allow_headers=["*"],              
+)
+
 
 app.include_router(brand)

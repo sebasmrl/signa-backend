4997b03197c6f05927e5a938f1f982738d4c0e78
Sebastian Morales <davesebastian99@gmail.com>
2025-08-22 21:21:53 -0500

    Modified: db.py

--- config/db.py
+++ config/db.py
@@ -1,9 +1,8 @@
 import os
 from sqlalchemy import create_engine, MetaData
 
-sqlite_filename = '../db/database.sqlite'
 base_dir = os.path.dirname(os.path.realpath(__file__))
-database_url = f"sqlite:///{os.path.join(base_dir, sqlite_filename)}"
+database_url = f"sqlite:///{os.path.join(base_dir, '..', 'db', 'database.sqlite')}"
 
 DATABASE_URL = os.getenv("DATABASE_URL", database_url)
 
